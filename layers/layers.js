040cb16708a6cf8af1ba47ed960b1be84d4f7adf
mahardikayurico <mahardikayurico.my@gmail.com>
2024-04-28 15:02:43 +0700

    first commit

---
+++ layers/layers.js
@@ -0,0 +1,58 @@
+ol.proj.proj4.register(proj4);
+ol.proj.get("EPSG:9518").setExtent([109.682758, -7.666933, 110.754552, -6.981350]);
+var wms_layers = [];
+
+
+        var lyr_OpenStreetMap_0 = new ol.layer.Tile({
+            'title': 'OpenStreetMap',
+            //'type': 'base',
+            'opacity': 1.000000,
+            
+            
+            source: new ol.source.XYZ({
+    attributions: ' ',
+                url: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png'
+            })
+        });
+var format_ADMINISTRASIDESA_AR_25K_1 = new ol.format.GeoJSON();
+var features_ADMINISTRASIDESA_AR_25K_1 = format_ADMINISTRASIDESA_AR_25K_1.readFeatures(json_ADMINISTRASIDESA_AR_25K_1, 
+            {dataProjection: 'EPSG:4326', featureProjection: 'EPSG:9518'});
+var jsonSource_ADMINISTRASIDESA_AR_25K_1 = new ol.source.Vector({
+    attributions: ' ',
+});
+jsonSource_ADMINISTRASIDESA_AR_25K_1.addFeatures(features_ADMINISTRASIDESA_AR_25K_1);
+var lyr_ADMINISTRASIDESA_AR_25K_1 = new ol.layer.Vector({
+                declutter: false,
+                source:jsonSource_ADMINISTRASIDESA_AR_25K_1, 
+                style: style_ADMINISTRASIDESA_AR_25K_1,
+                popuplayertitle: "ADMINISTRASIDESA_AR_25K",
+                interactive: true,
+                title: '<img src="styles/legend/ADMINISTRASIDESA_AR_25K_1.png" /> ADMINISTRASIDESA_AR_25K'
+            });
+var format_AGRISAWAH_AR_25K_2 = new ol.format.GeoJSON();
+var features_AGRISAWAH_AR_25K_2 = format_AGRISAWAH_AR_25K_2.readFeatures(json_AGRISAWAH_AR_25K_2, 
+            {dataProjection: 'EPSG:4326', featureProjection: 'EPSG:9518'});
+var jsonSource_AGRISAWAH_AR_25K_2 = new ol.source.Vector({
+    attributions: ' ',
+});
+jsonSource_AGRISAWAH_AR_25K_2.addFeatures(features_AGRISAWAH_AR_25K_2);
+var lyr_AGRISAWAH_AR_25K_2 = new ol.layer.Vector({
+                declutter: false,
+                source:jsonSource_AGRISAWAH_AR_25K_2, 
+                style: style_AGRISAWAH_AR_25K_2,
+                popuplayertitle: "AGRISAWAH_AR_25K",
+                interactive: true,
+                title: '<img src="styles/legend/AGRISAWAH_AR_25K_2.png" /> AGRISAWAH_AR_25K'
+            });
+
+lyr_OpenStreetMap_0.setVisible(true);lyr_ADMINISTRASIDESA_AR_25K_1.setVisible(true);lyr_AGRISAWAH_AR_25K_2.setVisible(true);
+var layersList = [lyr_OpenStreetMap_0,lyr_ADMINISTRASIDESA_AR_25K_1,lyr_AGRISAWAH_AR_25K_2];
+lyr_ADMINISTRASIDESA_AR_25K_1.set('fieldAliases', {'KDPPUM': 'KDPPUM', 'NAMOBJ': 'NAMOBJ', 'REMARK': 'REMARK', 'KDPBPS': 'KDPBPS', 'FCODE': 'FCODE', 'LUASWH': 'LUASWH', 'UUPP': 'UUPP', 'SRS_ID': 'SRS_ID', 'LCODE': 'LCODE', 'METADATA': 'METADATA', 'KDEBPS': 'KDEBPS', 'KDEPUM': 'KDEPUM', 'KDCBPS': 'KDCBPS', 'KDCPUM': 'KDCPUM', 'KDBBPS': 'KDBBPS', 'KDBPUM': 'KDBPUM', 'WADMKD': 'WADMKD', 'WIADKD': 'WIADKD', 'WADMKC': 'WADMKC', 'WIADKC': 'WIADKC', 'WADMKK': 'WADMKK', 'WIADKK': 'WIADKK', 'WADMPR': 'WADMPR', 'WIADPR': 'WIADPR', 'TIPADM': 'TIPADM', 'SHAPE_Leng': 'SHAPE_Leng', 'SHAPE_Area': 'SHAPE_Area', });
+lyr_AGRISAWAH_AR_25K_2.set('fieldAliases', {'NAMOBJ': 'NAMOBJ', 'JNSSWH': 'JNSSWH', 'FCODE': 'FCODE', 'AQDATE': 'AQDATE', 'PUDATE': 'PUDATE', 'REMARK': 'REMARK', 'KODLCO': 'KODLCO', 'SRS_ID': 'SRS_ID', 'LCODE': 'LCODE', 'METADATA': 'METADATA', 'TNMSWH': 'TNMSWH', 'SHAPE_Leng': 'SHAPE_Leng', 'SHAPE_Area': 'SHAPE_Area', });
+lyr_ADMINISTRASIDESA_AR_25K_1.set('fieldImages', {'KDPPUM': '', 'NAMOBJ': '', 'REMARK': '', 'KDPBPS': '', 'FCODE': '', 'LUASWH': '', 'UUPP': '', 'SRS_ID': '', 'LCODE': '', 'METADATA': '', 'KDEBPS': '', 'KDEPUM': '', 'KDCBPS': '', 'KDCPUM': '', 'KDBBPS': '', 'KDBPUM': '', 'WADMKD': '', 'WIADKD': '', 'WADMKC': '', 'WIADKC': '', 'WADMKK': '', 'WIADKK': '', 'WADMPR': '', 'WIADPR': '', 'TIPADM': '', 'SHAPE_Leng': '', 'SHAPE_Area': '', });
+lyr_AGRISAWAH_AR_25K_2.set('fieldImages', {'NAMOBJ': '', 'JNSSWH': '', 'FCODE': '', 'AQDATE': '', 'PUDATE': '', 'REMARK': '', 'KODLCO': '', 'SRS_ID': '', 'LCODE': '', 'METADATA': '', 'TNMSWH': '', 'SHAPE_Leng': '', 'SHAPE_Area': '', });
+lyr_ADMINISTRASIDESA_AR_25K_1.set('fieldLabels', {'KDPPUM': 'no label', 'NAMOBJ': 'no label', 'REMARK': 'no label', 'KDPBPS': 'no label', 'FCODE': 'no label', 'LUASWH': 'no label', 'UUPP': 'no label', 'SRS_ID': 'no label', 'LCODE': 'no label', 'METADATA': 'no label', 'KDEBPS': 'no label', 'KDEPUM': 'no label', 'KDCBPS': 'no label', 'KDCPUM': 'no label', 'KDBBPS': 'no label', 'KDBPUM': 'no label', 'WADMKD': 'no label', 'WIADKD': 'no label', 'WADMKC': 'no label', 'WIADKC': 'no label', 'WADMKK': 'no label', 'WIADKK': 'no label', 'WADMPR': 'no label', 'WIADPR': 'no label', 'TIPADM': 'no label', 'SHAPE_Leng': 'no label', 'SHAPE_Area': 'no label', });
+lyr_AGRISAWAH_AR_25K_2.set('fieldLabels', {'NAMOBJ': 'no label', 'JNSSWH': 'no label', 'FCODE': 'no label', 'AQDATE': 'no label', 'PUDATE': 'no label', 'REMARK': 'no label', 'KODLCO': 'no label', 'SRS_ID': 'no label', 'LCODE': 'no label', 'METADATA': 'no label', 'TNMSWH': 'no label', 'SHAPE_Leng': 'no label', 'SHAPE_Area': 'no label', });
+lyr_AGRISAWAH_AR_25K_2.on('precompose', function(evt) {
+    evt.context.globalCompositeOperation = 'normal';
+});
